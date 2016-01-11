cd3d15520cf8ca705477ad284ef5ef7402fd46bb
jordi <jordi@localhost>
2016-01-11 15:56:11 +0100

    Update

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 var io = require('socket.io').listen(3030);
 io.set('origins', '*:*');
 objects = [];
+mapping = [];
 
-
+players = 0;
 io.sockets.on('connection', function (socket) {
+
     socket.on('update', function(data){
      
-        getPlayer = existsPlayer(data.name);
-        if(getPlayer === false){
-            objects.push(data);
-        }else{
-            objects[getPlayer] = data;
-        }
+        
+       if(mapping[socket.id] == undefined){
+            objects[players]    = data;
+            mapping[socket.id]  = players;
+            players++;
+       }else{
+            pId = mapping[socket.id];
+            objects[pId] = data;
+       }
         
         socket.emit("update", objects);
     });
+
+    
+
     
 });
 
-function existsPlayer(name){
+/*function existsPlayer(name){
     
     for(var i = 0; i<objects.length; i++){
         var actName = objects[i].name;
@@ -26,5 +34,5 @@ function existsPlayer(name){
     }
     
     return false;
-}
+}*/
  
